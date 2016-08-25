a52d43cc402c06ddc1dc0fda00440faaf3f31f62
Matteo Melli <matteom@8kdata.com>
2016-08-25 17:55:13 +0200

    Added mongo client wrapper SSL and authentication configuration

--- client/driver-wrapper/src/main/java/com/eightkdata/mongowp/client/wrapper/MongoClientWrapper.java
+++ client/driver-wrapper/src/main/java/com/eightkdata/mongowp/client/wrapper/MongoClientWrapper.java
@@ -1,22 +1,28 @@
 
 package com.eightkdata.mongowp.client.wrapper;
 
-import com.eightkdata.mongowp.MongoVersion;
-import com.eightkdata.mongowp.client.core.MongoClient;
-import com.eightkdata.mongowp.client.core.MongoConnection;
-import com.eightkdata.mongowp.client.core.UnreachableMongoServerException;
-import com.google.common.base.Preconditions;
-import com.google.common.net.HostAndPort;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+
 import org.bson.BsonDocument;
 import org.bson.BsonDouble;
 import org.bson.Document;
 import org.bson.codecs.DocumentCodec;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
+
+import com.eightkdata.mongowp.MongoVersion;
+import com.eightkdata.mongowp.client.core.MongoClient;
+import com.eightkdata.mongowp.client.core.MongoConnection;
+import com.eightkdata.mongowp.client.core.UnreachableMongoServerException;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.net.HostAndPort;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 /**
  *
@@ -29,14 +35,17 @@ public class MongoClientWrapper implements MongoClient {
     private final com.mongodb.MongoClient driverClient;
     private final CodecRegistry codecRegistry;
 
-    public MongoClientWrapper(HostAndPort address) throws UnreachableMongoServerException {
-        testAddress(address);
+    public MongoClientWrapper(HostAndPort address, MongoClientOptions mongoClientOptions, MongoCredential credential) throws UnreachableMongoServerException {
+        testAddress(address, mongoClientOptions);
 
         this.address = address.withDefaultPort(27017);
 
         this.driverClient = new com.mongodb.MongoClient(
+            new ServerAddress(
                 this.address.getHostText(),
-                this.address.getPort()
+                this.address.getPort()),
+            credential != null ? ImmutableList.of(credential) : ImmutableList.of(),
+            mongoClientOptions
         );
 
         version = calculateVersion();
@@ -49,11 +58,11 @@ public class MongoClientWrapper implements MongoClient {
         return true;
     }
 
-    private void testAddress(HostAndPort address) throws UnreachableMongoServerException {
+    private void testAddress(HostAndPort address, MongoClientOptions mongoClientOptions) throws UnreachableMongoServerException {
         SocketAddress sa = new InetSocketAddress(address.getHostText(), address.getPort());
         Socket s = null;
         try {
-            s = new Socket();
+            s = mongoClientOptions.getSocketFactory().createSocket();
             s.connect(sa, 3000);
         } catch (IOException ex) {
             throw new UnreachableMongoServerException(address, ex);
