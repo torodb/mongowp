9b1868a6b108f40747accd73da0c556879177ba5
Gonzalo Ortiz Jaureguizar <gortiz@8kdata.com>
2017-06-12 11:12:11 +0200

    Fixed some new Checkstyle errors

--- mongowp-core/src/main/java/com/torodb/mongowp/utils/BsonDocumentBuilder.java
+++ mongowp-core/src/main/java/com/torodb/mongowp/utils/BsonDocumentBuilder.java
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.torodb.mongowp.utils;
 
+package com.torodb.mongowp.utils;
 
 import static com.torodb.mongowp.bson.utils.DefaultBsonValues.NULL;
 import static com.torodb.mongowp.bson.utils.DefaultBsonValues.newArray;
